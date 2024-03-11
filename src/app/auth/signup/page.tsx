f89b1de99dd6f95717dd11943f74c1d8d4265fdd
Logan Qiu <chiu.logan@gmail.com>
2024-03-11 14:51:31 -0700

    separate reusable agreement out

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,3 +1,4 @@
+import Agreement from "@/components/ui/authentication/agreement";
 import { UserRegisterForm } from "@/components/ui/authentication/user-register-form";
 import Link from "next/link";
 import React from "react";
@@ -15,23 +16,7 @@ function page() {
           </p>
         </div>
         <UserRegisterForm />
-        <p className="px-8 text-center text-sm text-muted-foreground">
-          By clicking continue, you agree to our{" "}
-          <Link
-            href="/terms"
-            className="underline underline-offset-4 hover:text-primary"
-          >
-            Terms of Service
-          </Link>{" "}
-          and{" "}
-          <Link
-            href="/privacy"
-            className="underline underline-offset-4 hover:text-primary"
-          >
-            Privacy Policy
-          </Link>
-          .
-        </p>
+        <Agreement />
       </div>
     </div>
   );
---
+++ src/components/ui/authentication/Agreement.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+function Agreement() {
+  return (
+    <>
+      <p className="px-8 text-center text-sm text-muted-foreground">
+        By clicking continue, you agree to our{" "}
+        <Link
+          href="/terms"
+          className="underline underline-offset-4 hover:text-primary"
+        >
+          Terms of Service
+        </Link>{" "}
+        and{" "}
+        <Link
+          href="/privacy"
+          className="underline underline-offset-4 hover:text-primary"
+        >
+          Privacy Policy
+        </Link>
+        .
+      </p>
+    </>
+  );
+}
+
+export default Agreement;
