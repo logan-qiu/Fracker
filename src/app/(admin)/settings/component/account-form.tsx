3f70f576d030225e46f9383e403b3aa4ef54695b
Logan Qiu <chiu.logan@gmail.com>
2024-03-28 00:32:37 -0700

    completed partial implementation of the account form and profile form, updated some wrongful imports

--- src/app/(admin)/settings/component/account-form.tsx
+++ src/app/(admin)/settings/component/account-form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+
 import {
   Form,
   FormControl,
@@ -14,22 +14,31 @@ import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { Calendar } from "@/components/ui/calendar";
+import { useState } from "react";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 function AccountForm() {
   const accountFormSchema = z.object({
     displayName: z
       .string()
       .min(2, {
-        message: "username must be at least 4 characters.",
+        message: "display must be at least 2 characters.",
       })
-      .max(20, { message: "username must not be more than 20 characters." }),
+      .max(20, { message: "display must not be more than 20 characters." }),
     dob: z.date({ required_error: "Date of birth is required." }),
   });
 
   type AccountFormValues = z.infer<typeof accountFormSchema>;
 
   const form = useForm<AccountFormValues>({
-    defaultValues: {},
+    defaultValues: {
+        displayName: '',
+        dob: new Date()
+    },
     resolver: zodResolver(accountFormSchema),
     mode: "onChange",
   });
@@ -38,8 +47,9 @@ function AccountForm() {
     console.log(values);
   };
 
+  const [isCalendarOpen, setIsCalendarOpen] = useState(false);
+
   return (
-    <>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
           <FormField
@@ -63,20 +73,49 @@ function AccountForm() {
             control={form.control}
             name="dob"
             render={({ field }) => (
-              <FormItem>
+                <FormItem className="flex flex-col">
                 <FormLabel>Date of birth</FormLabel>
-                <FormControl>
-                  {/* <Input placeholder="enter your bio here" {...field} /> */}
-                </FormControl>
+                <Popover open={isCalendarOpen} onOpenChange={setIsCalendarOpen}>
+                  <PopoverTrigger asChild>
+                    <FormControl>
+                      <Button
+                        variant={"outline"}
+                        className={cn(
+                          "w-[240px] pl-3 text-left font-normal",
+                          !field.value && "text-muted-foreground"
+                        )}
+                      >
+                        {field.value ? (
+                          format(field.value, "PPP")
+                        ) : (
+                          <span>Pick a date</span>
+                        )}
+                        <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                      </Button>
+                    </FormControl>
+                  </PopoverTrigger>
+                  <PopoverContent className="w-auto p-0" align="start">
+                    <Calendar
+                      mode="single"
+                      selected={field.value}
+                      onSelect={(e) => { field.onChange(e); setIsCalendarOpen(false); }}
+                      disabled={(date: Date) =>
+                        date > new Date() || date < new Date("1900-01-01")
+                      }
+                      initialFocus
+                    />
+                  </PopoverContent>
+                </Popover>
+                <FormDescription>
+                  Your date of birth is used to calculate your age.
+                </FormDescription>
                 <FormMessage />
-                <FormDescription>Your birthday is only used to calculate your age.</FormDescription>
               </FormItem>
             )}
           />
           <Button>Save Change</Button>
         </form>
       </Form>
-    </>
   );
 }
 
--- src/app/(admin)/settings/component/profile-form.tsx
+++ src/app/(admin)/settings/component/profile-form.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+
 import {
   Form,
   FormControl,
@@ -23,58 +23,63 @@ function ProfileForm() {
         message: "username must be at least 4 characters.",
       })
       .max(20, { message: "username must not be more than 20 characters." }),
-    email: z.string().email(),
-    bio: z.string()
+    // email: z.string().email(),
+    bio: z.string(),
   });
 
-  type ProfileFormValues = z.infer<typeof profileFormSchema>
+  type ProfileFormValues = z.infer<typeof profileFormSchema>;
 
   const form = useForm<ProfileFormValues>({
-    defaultValues: {},
+    defaultValues: {
+      username: "",
+      // email: "",
+      bio: "",
+    },
     resolver: zodResolver(profileFormSchema),
-    mode: 'onChange'
+    mode: "onChange",
   });
 
   const onSubmit = (values: ProfileFormValues) => {
     console.log(values);
-  }
+  };
 
   return (
-    <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-                control={form.control}
-                name="username"
-                render={({field}) => (
-                    <FormItem>
-                        <FormLabel>Username</FormLabel>
-                        <FormControl>
-                            <Input placeholder='username' {...field} />
-                        </FormControl>
-                        <FormMessage />
-                        <FormDescription>This is your public display name. It can be your real name or a pseudonym. You can only change this once every 30 days.</FormDescription>
-                    </FormItem>
-                )}
-            />
-            <FormField
-                control={form.control}
-                name="bio"
-                render={({field}) => (
-                    <FormItem>
-                        <FormLabel>Bio</FormLabel>
-                        <FormControl>
-                            <Input placeholder='enter your bio here' {...field} />
-                        </FormControl>
-                        <FormMessage />
-                        <FormDescription>This is your public bio.</FormDescription>
-                    </FormItem>
-                )}
-            />
-            <Button>Save Change</Button>
-        </form>
-      </Form>
-    </>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="username" {...field} />
+              </FormControl>
+              <FormMessage />
+              <FormDescription>
+                This is your public display name. It can be your real name or a
+                pseudonym. You can only change this once every 30 days.
+              </FormDescription>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="bio"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Bio</FormLabel>
+              <FormControl>
+                <Input placeholder="enter your bio here" {...field} />
+              </FormControl>
+              <FormMessage />
+              <FormDescription>This is your public bio.</FormDescription>
+            </FormItem>
+          )}
+        />
+        <Button>Save Change</Button>
+      </form>
+    </Form>
   );
 }
 
