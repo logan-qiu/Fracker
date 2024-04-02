4cc0ba41a64e29e26341da046126a504a14adb6c
Logan Qiu <chiu.logan@gmail.com>
2024-04-02 11:49:47 -0700

    added the cover for transctions page

---
+++ src/app/(admin)/transactions/page.tsx
@@ -0,0 +1,24 @@
+const TransactionsPage = async () => {
+  return (
+    <>
+      <div className="hidden h-full flex-1 flex-col space-y-8 p-8 md:flex">
+        <div className="flex items-center justify-between space-y-2">
+          <div>
+            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
+            <p className="text-muted-foreground">
+              Here&apos;s a list of your tasks for this month!
+            </p>
+          </div>
+          <div className="flex items-center space-x-2">
+            {/* <UserNav /> */}
+            usernav
+          </div>
+        </div>
+        {/* <DataTable data={tasks} columns={columns} /> */}
+        DataTable
+      </div>
+    </>
+  );
+};
+
+export default TransactionsPage;
